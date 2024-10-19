dfe175822b3feed93568a2002c884b3e5d41c389
Boodenator <boode1000@gmail.com>
2024-10-19 06:44:21 -0700

    script

--- markdown2html.py
+++ markdown2html.py
@@ -1,61 +1,97 @@
 #!/usr/bin/python3
-''' Code markdown2html.py script to take argument 2 strings:
-    First argument is name of the Markdown file
-    Second argument is name of output file
+''' Code markdown2html.py script to takes an argument 2 strings:
+    1st argument is name of Markdown file
+    2nd argument is name of output file
 '''
-import os
+
 import sys
+import os.path
+import re
+import hashlib
 
 if __name__ == '__main__':
-    # Check number of arguments
     if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
-        sys.exit(1)
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
+        exit(1)
 
-    markdown_file = sys.argv[1]  # First argument: Markdown file
-    output_file = sys.argv[2]     # Second argument: Output file name
+    if not os.path.isfile(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        exit(1)
 
-    # Check if the Markdown file exists
-    if not os.path.exists(markdown_file):
-        print(f'Missing {markdown_file}', file=sys.stderr)
-        sys.exit(1)
+    with open(sys.argv[1]) as read:
+        with open(sys.argv[2], 'w') as html:
+            unordered_start, ordered_start, paragraph = False, False, False
+            # bold syntax
+            for line in read:
+                line = line.replace('**', '<b>', 1)
+                line = line.replace('**', '</b>', 1)
+                line = line.replace('__', '<em>', 1)
+                line = line.replace('__', '</em>', 1)
 
-    # Function to check if the provided Markdown file exists
-    def check_markdown_file_exists(markdown_file):
-    return os.path.exists(markdown_file)
+                # md5
+                md5 = re.findall(r'\[\[.+?\]\]', line)
+                md5_inside = re.findall(r'\[\[(.+?)\]\]', line)
+                if md5:
+                    line = line.replace(md5[0], hashlib.md5(
+                        md5_inside[0].encode()).hexdigest())
 
-    # Function to convert Markdown headings to HTML
-    def convert_markdown_to_html(markdown_file):
-    html_content = []  # List to store HTML lines
-    with open(markdown_file, 'r') as file:
-        for line in file:
-            line = line.rstrip()  # Remove trailing whitespace
-            if line.startswith('#'):
-                heading_level = line.count('#')  # Count the number of '#' to determine the heading level
-                heading_text = line[heading_level:].strip()  # Get the heading text
-                # Generate the corresponding HTML heading
-                html_content.append(f'<h{heading_level}>{heading_text}</h{heading_level}>')
-                return '\n'.join(html_content)  # Join the HTML lines into a single string
+                # remove C letter
+                remove_letter_c = re.findall(r'\(\(.+?\)\)', line)
+                remove_c_more = re.findall(r'\(\((.+?)\)\)', line)
+                if remove_letter_c:
+                    remove_c_more = ''.join(
+                        c for c in remove_c_more[0] if c not in 'Cc')
+                    line = line.replace(remove_letter_c[0], remove_c_more)
 
-    # Check number of arguments
-    if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
-        sys.exit(1)
+                length = len(line)
+                headings = line.lstrip('#')
+                heading_num = length - len(headings)
+                unordered = line.lstrip('-')
+                unordered_num = length - len(unordered)
+                ordered = line.lstrip('*')
+                ordered_num = length - len(ordered)
+                # Lists & Headings
+                if 1 <= heading_num <= 6:
+                    line = '<h{}>'.format(
+                        heading_num) + headings.strip() + '</h{}>\n'.format(
+                        heading_num)
 
-        markdown_file = sys.argv[1]  # First argument: Markdown file
-        output_file = sys.argv[2]     # Second argument: Output file name
+                if unordered_num:
+                    if not unordered_start:
+                        html.write('<ul>\n')
+                        unordered_start = True
+                    line = '<li>' + unordered.strip() + '</li>\n'
+                if unordered_start and not unordered_num:
+                    html.write('</ul>\n')
+                    unordered_start = False
 
-    # Check if the Markdown file exists
-    if not check_markdown_file_exists(markdown_file):
-    print(f'Missing {markdown_file}', file=sys.stderr)
-    sys.exit(1)
+                if ordered_num:
+                    if not ordered_start:
+                        html.write('<ol>\n')
+                        ordered_start = True
+                    line = '<li>' + ordered.strip() + '</li>\n'
+                if ordered_start and not ordered_num:
+                    html.write('</ol>\n')
+                    ordered_start = False
 
-    # Convert the Markdown file to HTML
-    html_output = convert_markdown_to_html(markdown_file)
+                if not (heading_num or unordered_start or ordered_start):
+                    if not paragraph and length > 1:
+                        html.write('<p>\n')
+                        paragraph = True
+                    elif length > 1:
+                        html.write('<br/>\n')
+                    elif paragraph:
+                        html.write('</p>\n')
+                        paragraph = False
 
-    # Write the HTML output to the specified output file
-    with open(output_file, 'w') as f:
-    f.write(html_output)
+                if length > 1:
+                    html.write(line)
 
-    # If SUCCESS, exit with code 0
-    sys.exit(0)
+            if unordered_start:
+                html.write('</ul>\n')
+            if ordered_start:
+                html.write('</ol>\n')
+            if paragraph:
+                html.write('</p>\n')
+    exit (0)
